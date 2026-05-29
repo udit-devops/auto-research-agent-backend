52af63b7a5330b8fa05eb98ac8753c36c5154f66
Udit <dakuaudit@gmail.com>
2026-05-29 17:38:14 +0530

    tested tavily , groq and all setup completed

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-from app.services.groq_service import llm
-response = llm.invoke("what is your 1st model")
-print(response)
+# from app.services.groq_service import llm
+# response = llm.invoke("what is your 1st model")
+# print(response)
+
+from tavily import TavilyClient
+
+from app.core.config import settings
+
+tavily_client = TavilyClient(api_key=settings.TAVILY_API_KEY)
+search = tavily_client.search("What is the name of nvidia's CEO?")
+print(search)
