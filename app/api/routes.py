fa49f3dc96ff4be1453ebf3074efb109a06e74fd
Udit <dakuaudit@gmail.com>
2026-06-07 23:32:47 +0530

    added

--- app/api/routes.py
+++ app/api/routes.py
@@ -1,8 +1,22 @@
 from fastapi import APIRouter
 from pydantic import BaseModel
+from app.graph.builder import app
 
+router = APIRouter()
+
+ 
 class ResearchRequest(BaseModel):
     topic:str
 
-router = APIRouter()
+@router.post("/research")
+def generate_report(request: ResearchRequest):
+    result = app.invoke(
+        {
+            "topic": request.topic
+        }
+    )
+
+    return{
+        "report": result["report"]
+    }
 
