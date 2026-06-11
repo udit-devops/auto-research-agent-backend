819d12f8166d88b3067f809cf80731e93397798f
Udit <dakuaudit@gmail.com>
2026-06-11 12:28:15 +0530

    added cors

--- app/main.py
+++ app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from app.api.routes import router
 from app.db.database import engine
-
+from fastapi.middleware.cors import CORSMiddleware
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:3000",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(router)
 @app.get("/")
 def home():
