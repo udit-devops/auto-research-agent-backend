71a7158d85251a7026112a899780dd39219ed532
Udit <dakuaudit@gmail.com>
2026-06-18 14:43:01 +0530

    added cors

--- app/main.py
+++ app/main.py
@@ -8,6 +8,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://research-agent-frontend-one.vercel.app/"
     ],
     allow_credentials=True,
     allow_methods=["*"],
